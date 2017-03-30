2b2b3ed0b5af9bb54b5fa66fd0068b19d146caab
BLF2 <blf20822@126.com>
2017-03-30 21:30:21 +0800

    add new table ClassInfo

--- schame.sql
+++ schame.sql
@@ -18,3 +18,7 @@ CREATE TABLE UserInfo(userNum VARCHAR(100) PRIMARY KEY,userName VARCHAR(50),user
 --  ALTER TABLE UserInfo ADD CONSTRAINT userInfoRoleFK FOREIGN KEY userRoleId REFERENCES UserRoleInfo(userRoleId) ON UPDATE CASCADE ON DELETE CASCADE;
 
 -- delete from UserInfo;delete from UserRoleInfo;delete from RuleInfo;delete from UserRoleRuleRelation;
+
+CREATE TABLE CLassInfo(classId VARCHAR(100) PRIMARY KEY,majorityName VARCHAR(50) NOT NULL,classGrade VARCHAR(10) NOT NULL,
+                      classNum VARCHAR(10) NOT NULL,majorityClass VARCHAR(80) NOT NULL,monitorId VARCHAR(100) NOT  NULL,
+                      classNote VARCHAR(512));
