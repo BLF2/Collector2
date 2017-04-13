99c648d93c51aa850591a8bd98c39d535bcac953
BLF2 <blf20822@126.com>
2017-04-13 17:07:17 +0800

    add interface for RuleInfo and implements it

--- src/main/java/net/blf2/service/IRuleService.java
+++ src/main/java/net/blf2/service/IRuleService.java
@@ -1,7 +1,18 @@
 package net.blf2.service;
 
+import net.blf2.entity.RuleInfo;
+
+import java.util.List;
+
 /**
  * Created by blf2 on 17-4-13.
+ * 权限service
  */
 public interface IRuleService {
+    void insertRuleInfo(RuleInfo ruleInfo);
+    void deleteRuleInfoByRuleId(String ruleId);
+    void deleteRuleInfoByRuleIds(List<String> ruleIds);
+    void updateRuleInfo(RuleInfo ruleInfo);
+    RuleInfo queryRuleInfoByRuleId(String ruleId);
+    List<RuleInfo> queryRuleInfoAll();
 }
--- src/main/java/net/blf2/service/impl/RuleServiceImpl.java
+++ src/main/java/net/blf2/service/impl/RuleServiceImpl.java
@@ -1,7 +1,77 @@
 package net.blf2.service.impl;
 
+import net.blf2.dao.IRuleDao;
+import net.blf2.entity.RuleInfo;
+import net.blf2.exception.DeleteException;
+import net.blf2.exception.InsertException;
+import net.blf2.exception.UpdateException;
+import net.blf2.service.IRuleService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import javax.annotation.Resource;
+import java.util.List;
+
 /**
  * Created by blf2 on 17-4-13.
+ * IRuleService的具体实现类
  */
-public class RuleServiceImpl {
+@Service("RuleServiceImpl")
+@Transactional(rollbackFor = RuntimeException.class)
+public class RuleServiceImpl implements IRuleService {
+
+    @Resource
+    private IRuleDao ruleDao;
+
+
+    @Override
+    public void insertRuleInfo(RuleInfo ruleInfo) {
+        try {
+            ruleDao.insertRuleInfo(ruleInfo);
+        }catch (Exception ex){
+            ex.printStackTrace();
+            throw new InsertException(this);
+        }
+    }
+
+    @Override
+    public void deleteRuleInfoByRuleId(String ruleId) {
+        try {
+            ruleDao.deleteRuleInfoByRuleId(ruleId);
+        }catch (Exception ex){
+            ex.printStackTrace();
+            throw new DeleteException(this);
+        }
+    }
+
+    @Override
+    public void deleteRuleInfoByRuleIds(List<String> ruleIds) {
+        try {
+            ruleDao.deleteRuleInfoByRuleIds(ruleIds);
+        }catch (Exception ex){
+            ex.printStackTrace();
+            throw new DeleteException(this);
+        }
+    }
+
+    @Override
+    public void updateRuleInfo(RuleInfo ruleInfo) {
+        try {
+            ruleDao.updateRuleInfo(ruleInfo);
+        }catch (Exception ex){
+            ex.printStackTrace();
+            throw new UpdateException(this);
+        }
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public RuleInfo queryRuleInfoByRuleId(String ruleId) {
+        return ruleDao.queryRuleInfoByRuleId(ruleId);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<RuleInfo> queryRuleInfoAll() {
+        return ruleDao.queryRuleInfoAll();
+    }
 }
