bb61827d126ff51a0ca5688199b4f86d2d836d81
BLF2 <blf20822@126.com>
2017-04-25 15:14:17 +0800

    dev user service interface and implement

--- src/main/java/net/blf2/service/IUserService.java
+++ src/main/java/net/blf2/service/IUserService.java
@@ -1,9 +1,20 @@
 package net.blf2.service;
 
+import net.blf2.entity.UserInfo;
+import java.util.List;
+
 /**
  * Created by blf2 on 17-4-13.
  * 用户service
  */
 public interface IUserService {
-
+    void insertUserInfo(UserInfo userInfo);//增加信息
+    void deleteUserInfoByUserNum(String userNum);//根据学号删除信息
+    void deleteUserInfoByUserNums(List<String> userNums);//根据多个学号删除多条信息
+    void updateUserInfo(UserInfo userInfo);//更新信息
+    UserInfo queryUserInfoByUserNum(String userNum);//根据学号查询信息
+    List<UserInfo> queryUserInfoAll();//查询所有信息
+    UserInfo queryUserInfoByUserPhone(String userPhone);//根据手机号查询信息
+    List<UserInfo> queryUserInfoByUserRoleId(String userRoleId);//根据权限查询信息
+    String queryUserNumByUserPhone(String userPhone);//根据用户手机号查询学号
 }
---
+++ src/main/java/net/blf2/service/impl/UserServiceImpl.java
@@ -0,0 +1,84 @@
+package net.blf2.service.impl;
+
+import net.blf2.dao.IUserDao;
+import net.blf2.entity.UserInfo;
+import net.blf2.exception.InsertException;
+import net.blf2.exception.UpdateException;
+import net.blf2.service.IUserService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * Created by blf2 on 17-4-25.
+ *用户service
+ */
+@Service("UserServiceImpl")
+@Transactional(rollbackFor = RuntimeException.class)
+public class UserServiceImpl implements IUserService{
+
+    @Resource
+    private IUserDao userDao;
+
+    @Override
+    public void insertUserInfo(UserInfo userInfo) {
+        try {
+            userDao.insertUserInfo(userInfo);
+        }catch (Exception ex){
+            ex.printStackTrace();
+            throw new InsertException(this);
+        }
+    }
+
+    @Override
+    public void deleteUserInfoByUserNum(String userNum) {
+        //TODO:删除用户信息
+    }
+
+    @Override
+    public void deleteUserInfoByUserNums(List<String> userNums) {
+        //TODO:删除多个用户信息
+    }
+
+    @Override
+    public void updateUserInfo(UserInfo userInfo) {
+        try {
+            userDao.updateUserInfo(userInfo);
+        }catch (Exception ex){
+            ex.printStackTrace();
+            throw new UpdateException(this);
+        }
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserInfo queryUserInfoByUserNum(String userNum) {
+        return userDao.queryUserInfoByUserNum(userNum);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<UserInfo> queryUserInfoAll() {
+        return userDao.queryUserInfoAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserInfo queryUserInfoByUserPhone(String userPhone) {
+        return userDao.queryUserInfoByUserPhone(userPhone);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<UserInfo> queryUserInfoByUserRoleId(String userRoleId) {
+        return userDao.queryUserInfoByUserRoleId(userRoleId);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public String queryUserNumByUserPhone(String userPhone) {
+        return userDao.queryUserNumByUserPhone(userPhone);
+    }
+}
