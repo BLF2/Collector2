31042a2999d4f27ef45778d1d24c9df2996004d1
BLF2 <blf20822@126.com>
2017-04-25 14:57:18 +0800

    add note for rule service interface

--- src/main/java/net/blf2/service/IRuleService.java
+++ src/main/java/net/blf2/service/IRuleService.java
@@ -9,10 +9,10 @@ import java.util.List;
  * 权限service
  */
 public interface IRuleService {
-    void insertRuleInfo(RuleInfo ruleInfo);
-    void deleteRuleInfoByRuleId(String ruleId);
-    void deleteRuleInfoByRuleIds(List<String> ruleIds);
-    void updateRuleInfo(RuleInfo ruleInfo);
-    RuleInfo queryRuleInfoByRuleId(String ruleId);
-    List<RuleInfo> queryRuleInfoAll();
+    void insertRuleInfo(RuleInfo ruleInfo);//插入权限信息
+    void deleteRuleInfoByRuleId(String ruleId);//删除权限信息根据id
+    void deleteRuleInfoByRuleIds(List<String> ruleIds);//删除权限根据多个id
+    void updateRuleInfo(RuleInfo ruleInfo);//更新权限
+    RuleInfo queryRuleInfoByRuleId(String ruleId);//根据Id查询权限名称
+    List<RuleInfo> queryRuleInfoAll();//查询所有权限
 }
