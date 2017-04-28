f5594a797ce61103799042209395c344fc868c51
BLF2 <blf20822@126.com>
2017-04-28 19:50:34 +0800

    add notes for IClassDao

--- src/main/java/net/blf2/dao/IClassDao.java
+++ src/main/java/net/blf2/dao/IClassDao.java
@@ -10,13 +10,13 @@ import java.util.List;
  * 班级操作接口
  */
 public interface IClassDao {
-    void insertClassInfo(ClassInfo classInfo);
-    void updateClassInfo(ClassInfo classInfo);
-    void deleteClassInfoByClassId(String classId);
-    void deleteClassInfoByClassIds(List<String> classIds);
-    ClassInfo queryClassInfoByClassId(String classId);
-    List<ClassInfo> queryClassInfoAll();
-    ClassInfo queryClassInfoByMonitorId(String monitorId);
-    String queryClassIdByMonitorId(String monitorId);
-    List<UserInfo> queryUserInfosByMojrityClass(String majorityClass);
+    void insertClassInfo(ClassInfo classInfo);//插入班级信息
+    void updateClassInfo(ClassInfo classInfo);//更新班级信息
+    void deleteClassInfoByClassId(String classId);//根据Id删除班级信息
+    void deleteClassInfoByClassIds(List<String> classIds);//根据多个Id删除对应的信息
+    ClassInfo queryClassInfoByClassId(String classId);//根据Id查询班级信息
+    List<ClassInfo> queryClassInfoAll();//查询所有班级信息
+    ClassInfo queryClassInfoByMonitorId(String monitorId);//根据班长Id查询班级信息
+    String queryClassIdByMonitorId(String monitorId);//根据班长Id查询班级Id
+    List<UserInfo> queryUserInfosByMojrityClass(String majorityClass);//根据专业班级查询本班所有人信息
 }
---
+++ src/main/java/net/blf2/service/IClassService.java
@@ -0,0 +1,7 @@
+package net.blf2.service;
+
+/**
+ * Created by blf2 on 17-4-28.
+ */
+public interface IClassService {
+}
