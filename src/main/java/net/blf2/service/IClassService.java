55bb4e6f0d5a2723c5edd76554421b2e3f609e12
BLF2 <blf20822@126.com>
2017-05-04 10:25:25 +0800

    programing service for class information

--- src/main/java/net/blf2/service/IClassService.java
+++ src/main/java/net/blf2/service/IClassService.java
@@ -1,7 +1,22 @@
 package net.blf2.service;
 
+import net.blf2.entity.ClassInfo;
+import net.blf2.entity.UserInfo;
+
+import java.util.List;
+
 /**
  * Created by blf2 on 17-4-28.
+ * 班级操作
  */
 public interface IClassService {
+    void insertClassInfo(ClassInfo classInfo);//插入班级信息
+    void updateClassInfo(ClassInfo classInfo);//更新班级信息
+    void deleteClassInfoByClassId(String classId);//根据Id删除班级信息
+    void deleteClassInfoByClassIds(List<String> classIds);//根据多个Id删除对应的信息
+    ClassInfo queryClassInfoByClassId(String classId);//根据Id查询班级信息
+    List<ClassInfo> queryClassInfoAll();//查询所有班级信息
+    ClassInfo queryClassInfoByMonitorId(String monitorId);//根据班长Id查询班级信息
+    String queryClassIdByMonitorId(String monitorId);//根据班长Id查询班级Id
+    List<UserInfo> queryUserInfosByMojrityClass(String majorityClass);//根据专业班级查询本班所有人信息
 }
--- src/main/java/net/blf2/service/impl/ClassServiceImpl.java
+++ src/main/java/net/blf2/service/impl/ClassServiceImpl.java
@@ -1,8 +1,96 @@
 package net.blf2.service.impl;
 
+import net.blf2.dao.IClassDao;
+import net.blf2.entity.ClassInfo;
+import net.blf2.entity.UserInfo;
+import net.blf2.exception.DeleteException;
+import net.blf2.exception.InsertException;
+import net.blf2.exception.UpdateException;
+import net.blf2.service.IClassService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.util.List;
+
 /**
  * Created by blf2 on 17-4-28.
+ * 班级操作service
  */
-public class ClassService {
-    
+@Service("ClassServiceImpl")
+@Transactional(rollbackFor = RuntimeException.class)
+public class ClassServiceImpl implements IClassService{
+
+    @Resource
+    private IClassDao classDao;
+
+    @Override
+    public void insertClassInfo(ClassInfo classInfo) {
+        try {
+            classDao.insertClassInfo(classInfo);
+        }catch (Exception ex){
+            ex.printStackTrace();
+            throw new InsertException(this);
+        }
+    }
+
+    @Override
+    public void updateClassInfo(ClassInfo classInfo) {
+        try {
+            classDao.updateClassInfo(classInfo);
+        }catch (Exception ex){
+            ex.printStackTrace();
+            throw new UpdateException(this);
+        }
+    }
+
+    @Override
+    public void deleteClassInfoByClassId(String classId) {
+        try {
+            classDao.deleteClassInfoByClassId(classId);
+        }catch (Exception ex){
+            ex.printStackTrace();
+            throw new DeleteException(this);
+        }
+    }
+
+    @Override
+    public void deleteClassInfoByClassIds(List<String> classIds) {
+        try {
+            classDao.deleteClassInfoByClassIds(classIds);
+        }catch (Exception ex){
+            ex.printStackTrace();
+            throw new DeleteException(this);
+        }
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public ClassInfo queryClassInfoByClassId(String classId) {
+        return classDao.queryClassInfoByClassId(classId);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<ClassInfo> queryClassInfoAll() {
+        return classDao.queryClassInfoAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public ClassInfo queryClassInfoByMonitorId(String monitorId) {
+        return classDao.queryClassInfoByMonitorId(monitorId);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public String queryClassIdByMonitorId(String monitorId) {
+        return classDao.queryClassIdByMonitorId(monitorId);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<UserInfo> queryUserInfosByMojrityClass(String majorityClass) {
+        return classDao.queryUserInfosByMojrityClass(majorityClass);
+    }
 }
