f6a52ec79cf054a02caf3583ff5ce292e4720141
BLF2 <blf20822@126.com>
2017-05-04 11:53:57 +0800

    add pakage scan

--- src/main/java/net/blf2/CollectorApplication.java
+++ src/main/java/net/blf2/CollectorApplication.java
@@ -3,7 +3,7 @@ package net.blf2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "net.blf2")
 public class CollectorApplication {
 
 	public static void main(String[] args) {
