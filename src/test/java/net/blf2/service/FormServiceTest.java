0fd8136cf390fce9cefaf9586819c48cabfe1f7d
BLF2 <blf20822@126.com>
2017-05-07 00:26:28 +0800

    test for information

--- src/test/java/net/blf2/service/FormServiceTest.java
+++ src/test/java/net/blf2/service/FormServiceTest.java
@@ -1,7 +1,27 @@
 package net.blf2.service;
 
+import net.blf2.entity.PrimaryKeyValue;
+import net.blf2.service.impl.FormService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
 /**
  * Created by blf2 on 17-5-4.
+ * 测试 form service
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@MapperScan(basePackages="net.blf2.dao")
 public class FormServiceTest {
+    @Resource
+    private FormService formService;
+    @Test
+    public void testFormService(){
+        PrimaryKeyValue primaryKeyValue = new PrimaryKeyValue();
+    }
 }
