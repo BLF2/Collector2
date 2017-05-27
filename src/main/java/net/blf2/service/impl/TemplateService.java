8bc5307a01749b8ce945f80bc416b5604ab432f4
BLF2 <blf20822@126.com>
2017-05-27 14:56:36 +0800

    add new model for div key value

--- src/main/java/net/blf2/service/impl/TemplateService.java
+++ src/main/java/net/blf2/service/impl/TemplateService.java
@@ -1,7 +1,75 @@
 package net.blf2.service.impl;
 
+import com.google.gson.Gson;
+import net.blf2.dao.MongoOperator;
+import net.blf2.entity.InfoTemplateForm;
+import net.blf2.util.Consts;
+import org.bson.Document;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
 /**
  * Created by blf2 on 17-5-25.
  */
+@Service("TemplateService")
 public class TemplateService {
+    @Autowired
+    private Gson gson;
+
+    private String databaseName = Consts.MONGO_DATABASE_NAME;
+    private String collectionName = Consts.COLLECTION_FOR_FORM;
+    private String collectionForResult = Consts.FORM_RESULT_COLLECTION_NAME;
+
+    public boolean insertInfoTemplateForm(InfoTemplateForm infoTemplateForm){
+        if(infoTemplateForm == null)
+            return false;
+        Map<String,Object>dataMap = new HashMap<>();
+        dataMap.put("_id",infoTemplateForm.getTemplateId());
+        dataMap.put(Consts.DIY_FORM_INFO,gson.toJson(infoTemplateForm));
+        return MongoOperator.insertDocument(databaseName,collectionName,dataMap);
+    }
+    public boolean deleteInfoTemplateFormById(String _id){
+        if(_id == null || _id.isEmpty())
+            return false;
+        Map<String,Object>delMap = new HashMap<>();
+        delMap.put("_id",_id);
+        return MongoOperator.deleteDocumentById(databaseName,collectionName,delMap);
+    }
+    public boolean updateInfoTemplate(InfoTemplateForm infoTemplateForm){
+        if(infoTemplateForm == null)
+            return false;
+        Map<String,Object>updMap = new HashMap<>();
+        updMap.put("_id",infoTemplateForm.getTemplateId());
+        updMap.put(Consts.DIY_FORM_INFO,gson.toJson(infoTemplateForm));
+       return MongoOperator.updateDocument(databaseName,collectionName,updMap);
+    }
+    public InfoTemplateForm queryInfoTemplateById(String infoTemplateId){
+        if(infoTemplateId == null)
+            return null;
+        Map<String,Object>queryMap = new HashMap<>();
+        queryMap.put("_id",infoTemplateId);
+        Document queryDocument = MongoOperator.findDocumentById(databaseName,collectionName,queryMap);
+        if(queryDocument != null && queryDocument.get(Consts.DIY_FORM_INFO) != null){
+            InfoTemplateForm infoTemplateForm = gson.fromJson((String)queryDocument.get(Consts.DIY_FORM_INFO),InfoTemplateForm.class);
+            return infoTemplateForm;
+        }
+        return null;
+    }
+    public List<InfoTemplateForm> queryInfoTemplateFormAll(){
+        List<Document> documentListAll = new LinkedList<>();
+        List<InfoTemplateForm> infoTemplateFormAll = new LinkedList<>();
+        documentListAll = MongoOperator.findAllDocuments(databaseName,collectionName);
+        if(documentListAll != null){
+            for(Document document : documentListAll){
+                if(document != null && document.get(Consts.DIY_FORM_INFO) != null){
+                    infoTemplateFormAll.add(gson.fromJson((String)document.get(Consts.DIY_FORM_INFO),InfoTemplateForm.class));
+                }
+            }
+            return infoTemplateFormAll.size() > 0 ? infoTemplateFormAll : null;
+        }
+        return null;
+    }
+
 }
--- src/main/resources/static/js/CandidateWord.js
+++ src/main/resources/static/js/CandidateWord.js
@@ -1,65 +1,150 @@
-index = 1;
-flag = false;
-function addNewHxxForSingel() {
-    if(!checkExistsForhxx())
-        addNewHouxuanxiang();
-}
-function checkExistsForhxx() {
-    if(!flag){
-        flag = true;
-        return false;
-    }
-    for(var i = 1; i < index;i++){
-        var checkDiv = document.getElementById("houxuanxiang"+i);
-        if(checkDiv != undefined)
-            return true;
-    }
-    return false;
-}
-function addNewHouxuanxiang() {
-    var houxuanxiangwz = document.getElementById("houxuanxiangwz");
+var indexForKeyValue = 1;
+function chooseToSelect(ele) {
+    var index = ele.selectedIndex;
+    var sel = ele.options[index].value;
+    if(sel == 'text' || sel == 'date')
+        deleteInputForValueCondition(ele);
+    else if(sel == 'single' || sel == 'multiple')
+        addInputForValueCondition(ele);
+    else if(sel == 'forceKeyValue')
+        addTextAreaForValue(ele);
 
-    var newP = document.createElement("p");
-    newP.innerHTML="候选项：";
-    var newInput = document.createElement("input");
-    newInput.setAttribute("class","form-control");
-    newInput.setAttribute("type","text");
-    newInput.setAttribute("name","choices"+index);
-    newInput.setAttribute("id","houxuanValue"+index);
-    <!-- 增加按钮 -->
-    var newSpanForAdd = document.createElement("span");
-    newSpanForAdd.setAttribute("class","glyphicon glyphicon-plus");
-    newSpanForAdd.setAttribute("aria-hidden","true");
-    newSpanForAdd.innerHTML="增加一项";
-    var newButtonForAdd = document.createElement("button");
-    newButtonForAdd.setAttribute("type","button");
-    newButtonForAdd.setAttribute("class","btn btn-primary btn-sm");
-    newButtonForAdd.setAttribute("onclick","addNewHouxuanxiang()");
-    newButtonForAdd.appendChild(newSpanForAdd);
-    <!-- 删除按钮 -->
-    var newSpanForDel = document.createElement("span");
-    newSpanForDel.setAttribute("class","glyphicon glyphicon-trash");
-    newSpanForDel.setAttribute("aria-hidden","true");
-    newSpanForDel.innerHTML="删除本项";
-    var newButtonForDel = document.createElement("button");
-    newButtonForDel.setAttribute("type","button");
-    newButtonForDel.setAttribute("class","btn btn-danger btn-sm");
-    var delHxxId = "houxuanxiang"+index;
-    newButtonForDel.setAttribute("onclick","delHouxuanxiang('"+delHxxId+"')");
-    newButtonForDel.appendChild(newSpanForDel);
-
-    var newDiv = document.createElement("div");
-    newDiv.appendChild(newP);
-    newDiv.appendChild(newInput);
-    newDiv.appendChild(newButtonForAdd);
-    newDiv.appendChild(newButtonForDel);
-    newDiv.setAttribute("id","houxuanxiang"+index);
-    houxuanxiangwz.appendChild(newDiv);
-    index++;
 }
-function delHouxuanxiang(id) {
-    var delDiv = document.getElementById(id);
-    if(delDiv){
-        delDiv.parentNode.removeChild(delDiv);
+function deleteInputForValueCondition(ele) {
+    var id = ele.parentNode.parentNode.id;
+    var isExistsDiv = document.getElementById("valueCondition"+id);
+    if(isExistsDiv){
+        isExistsDiv.parentNode.removeChild(isExistsDiv);
     }
+}
+function addInputForValueCondition(ele) {
+    var id = ele.parentNode.parentNode.id;
+    var isExistsDiv = document.getElementById("valueCondition"+id);
+    var isExistsInput = document.getElementById("valueCondition"+id+"input");
+    var pele = document.getElementById(id);
+    if(isExistsDiv && !isExistsInput){
+        deleteInputForValueCondition(ele);
+    }else if(isExistsDiv && isExistsInput)
+        return ;
+    var div = document.createElement("div");
+    div.setAttribute("class","form-group");
+    div.setAttribute("id","valueCondition"+id);
+    var input = document.createElement("input");
+    input.setAttribute("type","text");
+    input.setAttribute("name","infoTemplateForm.infoTemplateList["+(indexForKeyValue-1)+"].conditionValue");
+    input.setAttribute("placeholder","多个项目请用英文逗号隔开");
+    input.setAttribute("class","form-control");
+    input.setAttribute("id","valueCondition"+id+"input");
+    div.appendChild(input);
+    pele.appendChild(div);
+}
+
+function addPrimaryIterm() {
+    //start div项目名
+    var lable1 = document.createElement("label");
+    lable1.innerHTML="项目名：";
+    var buttonForDel = document.createElement("button");
+    buttonForDel.setAttribute("type","button");
+    buttonForDel.setAttribute("class","btn btn-danger btn-sm");
+    buttonForDel.setAttribute("onclick","deleteThisIterm(this)");
+    var spanForDel = document.createElement("span");
+    spanForDel.setAttribute("class","glyphicon glyphicon-trash");
+    spanForDel.setAttribute("aria-hidden","true");
+    buttonForDel.appendChild(spanForDel);
+    var input1 = document.createElement("input");
+    input1.setAttribute("type","text");
+    input1.setAttribute("class","form-control");
+    input1.setAttribute("name","infoTemplateForm.infoTemplateList["+indexForKeyValue+"].itermName");
+    input1.setAttribute("placeholder","项目名");
+    var div1 = document.createElement("div");
+    div1.setAttribute("class","form-group");
+    div1.appendChild(lable1);
+    div1.appendChild(buttonForDel);
+    div1.appendChild(input1);
+    //end
+    //start div 项目约束
+    var label2 = document.createElement("label");
+    var input2 = document.createElement("input");
+    input2.setAttribute("type","checkbox");
+    input2.setAttribute("name","infoTemplateForm.infoTemplateList["+indexForKeyValue+"].itermCondition");
+    input2.setAttribute("value","RequiredInformation");
+    var divForStr1 = document.createElement("div");
+    divForStr1.innerHTML="必填（默认为选填）";
+    label2.appendChild(input2);
+    label2.appendChild(divForStr1);
+    var span1 = document.createElement("span");
+    span1.innerHTML ="项目约束";
+    var div2 = document.createElement("div");
+    div2.setAttribute("class","checkbox");
+    div2.appendChild(span1);
+    div2.appendChild(label2);
+    //end
+    //start 值约束
+    var span2 = document.createElement("span");
+    span2.innerHTML="值约束：";
+    var select1 = document.createElement("select");
+    select1.setAttribute("name","infoTemplateForm.infoTemplateList["+indexForKeyValue+"].valueCondition");
+    select1.setAttribute("onchange","chooseToSelect(this)");
+    var option1 = document.createElement("option");
+    option1.setAttribute("selected","selected");
+    option1.setAttribute("value","text");
+    option1.innerHTML="文本";
+    var option2 = document.createElement("option");
+    option2.setAttribute("value","date");
+    option2.innerHTML="日期";
+    var option3 = document.createElement("option");
+    option3.setAttribute("value","single");
+    option3.innerHTML="单选";
+    var option4 = document.createElement("option");
+    option4.setAttribute("value","multiple");
+    option4.innerHTML="多选";
+    var option5 = document.createElement("option");
+    option5.setAttribute("value","forceKeyValue");
+    option5.innerHTML="键值对";
+    select1.appendChild(option1);
+    select1.appendChild(option2);
+    select1.appendChild(option3);
+    select1.appendChild(option4);
+    select1.appendChild(option5);
+    var divForValueCondition = document.createElement("div");
+    divForValueCondition.setAttribute("class","select");
+    divForValueCondition.appendChild(span2);
+    divForValueCondition.appendChild(select1);
+    //end
+    //一个具体项目
+    var parent = document.getElementById("itemDiv0").parentNode;
+    var itemDiv = document.createElement("div");
+    itemDiv.appendChild(document.createElement("hr"));
+    itemDiv.setAttribute("id","itemDiv"+indexForKeyValue);
+    indexForKeyValue++;
+    itemDiv.appendChild(div1);
+    itemDiv.appendChild(div2);
+    itemDiv.appendChild(divForValueCondition);
+    parent.appendChild(itemDiv);
+}
+function addTextAreaForValue(ele) {
+    var id = ele.parentNode.parentNode.id;
+    var isExistsDiv = document.getElementById("valueCondition"+id);
+    var isExistsTextarea = document.getElementById("valueCondition"+id+"textarea");
+    var pele = document.getElementById(id);
+    if(isExistsDiv && !isExistsTextarea){
+        deleteInputForValueCondition(ele);
+    }else if(isExistsDiv && isExistsTextarea)
+        return ;
+    var div = document.createElement("div");
+    div.setAttribute("class","form-group");
+    div.setAttribute("id","valueCondition"+id);
+    var textarea = document.createElement("textarea");
+    textarea.setAttribute("class","form-control");
+    textarea.setAttribute("rows","3");
+    textarea.setAttribute("placeholder","键和值中间用#隔开，多个键值对用英文逗号隔开");
+    textarea.setAttribute("id","valueCondition"+id+"textarea")
+    textarea.setAttribute("name","infoTemplateForm.infoTemplateList["+(indexForKeyValue-1)+"].conditionValue");
+    div.appendChild(textarea);
+    pele.appendChild(div);
+}
+function deleteThisIterm(ele) {
+    var id = ele.parentNode.parentNode.id;
+    var delEle = document.getElementById(id);
+    delEle.parentNode.removeChild(delEle);
 }
